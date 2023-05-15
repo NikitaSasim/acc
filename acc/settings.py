71c3f201879175cadc60c54ffabf063f4ab65d33
NikitaSasim <101585298+NikitaSasim@users.noreply.github.com>
2023-05-16 01:54:52 +0200

    row base page

--- acc/settings.py
+++ acc/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'users',
     'ledger'
 ]
@@ -51,15 +52,15 @@ ROOT_URLCONF = 'acc.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Income, IncomeCategory, ExpensesCategory, Expenses
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "base.html"
+
+    def get(self, request):
+        incomes = Income.objects.filter(user=request.user)
+
+        params = {
+            'incomes': incomes
+        }
+
+        return render(request, self.template_name, params)
