1915491ce5e9f52c74b3053d18e9fd4887c510e7
NikitaSasim <101585298+NikitaSasim@users.noreply.github.com>
2023-05-15 23:48:40 +0200

    user created

--- acc/settings.py
+++ acc/settings.py
@@ -23,6 +23,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = 'users.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
