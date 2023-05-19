20519d98a557d7221daa3385991698931ab32de9
NikitaSasim <101585298+NikitaSasim@users.noreply.github.com>
2023-05-19 02:44:12 +0200

    --new income edded

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, CreateIncome
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="ledger-home")
+    path("", HomeView.as_view(), name="ledger-home"),
+    path("add_income/", CreateIncome.as_view(), name="add-income")
 ]
